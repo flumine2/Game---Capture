167f4682e52a033a24436d2fd4c44efc60284fdb
Savienkov Maksym <froggt25@gmail.com>
2022-05-24 22:46:06 +0300

    Add project files.

---
+++ Game - Capture/model/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game___Capture.model
+{
+    class Game
+    {
+        private int score;
+        private 
+    }
+}
